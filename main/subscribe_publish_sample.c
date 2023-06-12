1e09b37a206c089d32db1cbef04152000f902ac6
DaintyJet <Matther_harper30@aol.com>
2023-06-12 18:30:18 -0400

    Remove outdated component

--- main/subscribe_publish_sample.c
+++ main/subscribe_publish_sample.c
@@ -23,17 +23,6 @@
  * Some setup is required. See example README for details.
  *
  */
-
-/*
-Things to configure:
-WiFi SSID, password at example configuration of menuconfig
-AWS end point at component config of Amazon web services ...
-
-certificate.pem.crt
-private.pem.key
-
-Create certificate, policy at AWS IoT
-*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
@@ -58,6 +47,12 @@ Create certificate, policy at AWS IoT
 #include "aws_iot_log.h"
 #include "aws_iot_version.h"
 #include "aws_iot_mqtt_client_interface.h"
+
+#include <esp_idf_version.h>
+#if ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(4, 1, 0)
+// Features supported in 4.1+
+#define ESP_NETIF_SUPPORTED
+#endif
 
 static const char *TAG = "subpub";
 
@@ -119,37 +114,20 @@ char HostAddress[255] = AWS_IOT_MQTT_HOST;
  */
 uint32_t port = AWS_IOT_MQTT_PORT;
 
-
-static void event_handler(void* handler_args, esp_event_base_t base, int32_t id, void* event_data)
+static void event_handler(void* arg, esp_event_base_t event_base,
+                          int32_t event_id, void* event_data)
 {
-    ESP_LOGI(TAG, "event_handler: %s:%d", base, id);
-
-    if(base == WIFI_EVENT) {
-        switch(id) {
-        case WIFI_EVENT_STA_START:
-            esp_wifi_connect();
-            break;
-        case WIFI_EVENT_STA_CONNECTED:
-            break;
-        case WIFI_EVENT_STA_DISCONNECTED:
-            /* This is a workaround as ESP32 WiFi libs don't currently
-            auto-reassociate. */
-            esp_wifi_connect();
-            xEventGroupClearBits(wifi_event_group, CONNECTED_BIT);
-            break;
-        default:
-            break;
-        }
-    }
-    else if(base == IP_EVENT) {
-        switch(id) {
-        case IP_EVENT_STA_GOT_IP:
-            {
-                ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;
-                ESP_LOGI(TAG, "got ip:" IPSTR, IP2STR(&event->ip_info.ip));
-                xEventGroupSetBits(wifi_event_group, CONNECTED_BIT);
-            }
-        }
+    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
+        esp_wifi_connect();
+    } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
+        ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;
+        ESP_LOGI(TAG, "Connected with IP Address:" IPSTR, IP2STR(&event->ip_info.ip));
+        /* Signal main application to continue execution */
+        xEventGroupSetBits(wifi_event_group, CONNECTED_BIT);
+    } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
+        ESP_LOGI(TAG, "Disconnected. Connecting to the AP again...");
+        esp_wifi_connect();
+        xEventGroupClearBits(wifi_event_group, CONNECTED_BIT);
     }
 }
 
@@ -183,7 +161,7 @@ void disconnectCallbackHandler(AWS_IoT_Client *pClient, void *data) {
 void aws_iot_task(void *param) {
     char cPayload[100];
 
-    int32_t i = 0;
+    int i = 0;
 
     IoT_Error_t rc = FAILURE;
 
@@ -201,9 +179,9 @@ void aws_iot_task(void *param) {
     mqttInitParams.port = port;
 
 #if defined(CONFIG_EXAMPLE_EMBEDDED_CERTS)
-    mqttInitParams.pRootCALocation = (char *)aws_root_ca_pem_start;
-    mqttInitParams.pDeviceCertLocation = (char *)certificate_pem_crt_start;
-    mqttInitParams.pDevicePrivateKeyLocation = (char *)private_pem_key_start;
+    mqttInitParams.pRootCALocation = (const char *)aws_root_ca_pem_start;
+    mqttInitParams.pDeviceCertLocation = (const char *)certificate_pem_crt_start;
+    mqttInitParams.pDevicePrivateKeyLocation = (const char *)private_pem_key_start;
 
 #elif defined(CONFIG_EXAMPLE_FILESYSTEM_CERTS)
     mqttInitParams.pRootCALocation = ROOT_CA_PATH;
@@ -251,12 +229,12 @@ void aws_iot_task(void *param) {
     connectParams.clientIDLen = (uint16_t) strlen(CONFIG_AWS_EXAMPLE_CLIENT_ID);
     connectParams.isWillMsgPresent = false;
 
-    ESP_LOGI(TAG, "Connecting to AWS with client ID '%s'", connectParams.pClientID);
+    ESP_LOGI(TAG, "Connecting to AWS...");
     do {
         rc = aws_iot_mqtt_connect(&client, &connectParams);
         if(SUCCESS != rc) {
             ESP_LOGE(TAG, "Error(%d) connecting to %s:%d", rc, mqttInitParams.pHostURL, mqttInitParams.port);
-            vTaskDelay(1000 / portTICK_RATE_MS);
+            vTaskDelay(1000 / portTICK_PERIOD_MS);
         }
     } while(SUCCESS != rc);
 
@@ -300,8 +278,8 @@ void aws_iot_task(void *param) {
             continue;
         }
 
-        ESP_LOGI(TAG, "Stack remaining for task '%s' is %d bytes", pcTaskGetTaskName(NULL), uxTaskGetStackHighWaterMark(NULL));
-        vTaskDelay(1000 / portTICK_RATE_MS);
+        ESP_LOGI(TAG, "Stack remaining for task '%s' is %d bytes", pcTaskGetName(NULL), uxTaskGetStackHighWaterMark(NULL));
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
         sprintf(cPayload, "%s : %d ", "hello from ESP32 (QOS0)", i++);
         paramsQOS0.payloadLen = strlen(cPayload);
         rc = aws_iot_mqtt_publish(&client, TOPIC, TOPIC_LEN, &paramsQOS0);
@@ -321,23 +299,21 @@ void aws_iot_task(void *param) {
 
 static void initialise_wifi(void)
 {
-    wifi_event_group = xEventGroupCreate();
-    ESP_ERROR_CHECK(esp_netif_init());
-
+    /* Initialize TCP/IP */
+#ifdef ESP_NETIF_SUPPORTED
+    esp_netif_init();
+#else
+    tcpip_adapter_init();
+#endif
     ESP_ERROR_CHECK(esp_event_loop_create_default());
+    ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler, NULL));
+    ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &event_handler, NULL));
+    /* Initialize Wi-Fi including netif with default config */
+#ifdef ESP_NETIF_SUPPORTED
     esp_netif_create_default_wifi_sta();
+#endif
 
-    ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT,
-                                                        ESP_EVENT_ANY_ID,
-                                                        &event_handler,
-                                                        NULL,
-                                                        NULL));
-    ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT,
-                                                        IP_EVENT_STA_GOT_IP,
-                                                        &event_handler,
-                                                        NULL,
-                                                        NULL));
-
+    wifi_event_group = xEventGroupCreate();
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     ESP_ERROR_CHECK( esp_wifi_init(&cfg) );
     ESP_ERROR_CHECK( esp_wifi_set_storage(WIFI_STORAGE_RAM) );
